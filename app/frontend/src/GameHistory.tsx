a9b195db5cc56ee18ea2710b30bbdf9db9f1a192
2underscores <jeremy__smith@hotmail.com>
2024-08-11 11:32:21 +1000

    CICD test

--- app/frontend/src/GameHistory.tsx
+++ app/frontend/src/GameHistory.tsx
@@ -15,7 +15,7 @@ export default function GameHistory({selectedIndex, latestIndex, clickHandler}: 
     return (
         <>
             <div className="GameHistory">
-                3 Game History ({selectedIndex} selected)
+                4 Game History ({selectedIndex} selected)
                 <div>
                     {buttons}
                 </div>
