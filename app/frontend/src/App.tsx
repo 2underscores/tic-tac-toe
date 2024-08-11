6a4a349dec59a9732d0359a1afb000c2c40ba0bc
2underscores <jeremy__smith@hotmail.com>
2024-08-11 15:52:18 +1000

    Scored restyled and turn highlighted

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -5,18 +5,18 @@ import GameHistory from './GameHistory';
 
 const BOARD_SIZE = 3;
 const BLANK = 0; // TODO: change to null
-const players = [{val: 1, rep: 'X',}, {val: -1, rep: 'O',}]
+const players = [{ val: 1, rep: 'X', }, { val: -1, rep: 'O', }]
 
 type BoardType = ReadonlyArray<number>;
 type GameType = ReadonlyArray<BoardType>;
 type GameStatus = 'WIN_X' | 'WIN_O' | 'DRAW' | 'ONGOING';
-type GameScores = {x: number, o: number, draw: number};
+type GameScores = { x: number, o: number, draw: number };
 
 function App() {
-  const starter_board: BoardType = new Array(BOARD_SIZE*BOARD_SIZE).fill(BLANK);
+  const starter_board: BoardType = new Array(BOARD_SIZE * BOARD_SIZE).fill(BLANK);
   const [game, setGame] = useState<GameType>([starter_board])
   const [historyIndex, setHistoryIndex] = useState<number>(0);
-  const [scores, setScores] = useState<GameScores>({x: 0, o: 0, draw: 0});
+  const [scores, setScores] = useState<GameScores>({ x: 0, o: 0, draw: 0 });
 
   function turnClickHandler(clickIndex: number) {
     const new_board = game[historyIndex].slice();
@@ -34,8 +34,8 @@ function App() {
     // Make move
     const playerChar = historyIndex % 2 ? players[1].val : players[0].val;
     new_board[clickIndex] = playerChar;
-    setGame([...game.slice(0,historyIndex+1), new_board]);
-    setHistoryIndex(historyIndex+1);
+    setGame([...game.slice(0, historyIndex + 1), new_board]);
+    setHistoryIndex(historyIndex + 1);
   }
 
   function historyClickHandler(clickIndex: number) {
@@ -43,9 +43,9 @@ function App() {
   }
 
   function gameState(board: BoardType): GameStatus {
-    const winLineIndexes = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    const winLineIndexes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for (let winLine of winLineIndexes) {
-      let sum = winLine.reduce((acc, idx)=>acc+board[idx], 0);
+      let sum = winLine.reduce((acc, idx) => acc + board[idx], 0);
       if (sum === 3) {
         return 'WIN_X';
       } else if (sum === -3) {
@@ -60,11 +60,11 @@ function App() {
 
   function scoreGame(state: GameStatus): void {
     if (state === 'WIN_X') {
-      setScores({...scores, x: scores.x+1});
+      setScores({ ...scores, x: scores.x + 1 });
     } else if (state === 'WIN_O') {
-      setScores({...scores, o: scores.o+1});
+      setScores({ ...scores, o: scores.o + 1 });
     } else if (state === 'DRAW') {
-      setScores({...scores, draw: scores.draw+1});
+      setScores({ ...scores, draw: scores.draw + 1 });
     }
   }
 
@@ -73,17 +73,36 @@ function App() {
     setHistoryIndex(0);
   }
 
+  function highlightPlayerTurn(player: 'X' | 'O'): boolean {
+    if (gameState(game[historyIndex]) !== 'ONGOING') {
+      return false;
+    }
+    const playerTurn = historyIndex % 2 ? 'O' : 'X';
+    if(player !== playerTurn) {
+      return false;
+    }
+    return true;
+  }
+
   return (
     <>
-      <div>{gameState(game[historyIndex])}</div>
-      <Board board={game[historyIndex]} turnHandler={turnClickHandler}/>
-      <GameHistory selectedIndex={historyIndex} latestIndex={game.length-1} clickHandler={historyClickHandler}/>
-      <button onClick={resetBoard}>Reset</button>
-      <div>
-        <div>X: {scores.x}</div>
-        <div>O: {scores.o}</div>
-        <div>Draw: {scores.draw}</div>
+      <Board board={game[historyIndex]} turnHandler={turnClickHandler} />
+      <GameHistory selectedIndex={historyIndex} latestIndex={game.length - 1} clickHandler={historyClickHandler} />
+      <div className="scores-container">
+      <div className={`score-item ${highlightPlayerTurn('X') ? 'highlighted' : 'dimmed'}`}>
+          <div>Player X</div>
+          <div>{scores.x}</div>
+        </div>
+        <div className="score-item dimmed">
+          <div>Draw</div>
+          <div>{scores.draw}</div>
+        </div>
+        <div className={`score-item ${highlightPlayerTurn('O') ? 'highlighted' : 'dimmed'}`}>
+          <div>Player O</div>
+          <div>{scores.o}</div>
+        </div>
       </div>
+      {/* <button onClick={resetBoard}>Reset</button> */}
     </>
   )
 }
